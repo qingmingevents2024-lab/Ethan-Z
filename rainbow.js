5a21f538f5e98fdb842cbaaad55d40237e51101b
qingmingevents2024-lab <qingming.events.2024@gmail.com>
2026-07-30 19:19:06 -0400

    fj

--- rainbow.js
+++ rainbow.js
@@ -1,10 +1,9 @@
-let nothing = 0;
 const main = document.body;
 let angle=0;
 let increment=5;
 function doRainbow(){
     requestAnimationFrame(doRainbow);
-    main.style.backgroundColor = "hsl("+angle+"deg, 100%, %)";
+    main.style.backgroundColor = "hsl("+angle+"deg, 100%, 50%)";
     angle+=increment;  
 } 
 doRainbow();
